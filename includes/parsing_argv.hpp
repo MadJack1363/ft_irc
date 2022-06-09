e029b05900fe8500dd0fd4e1a6a11643f1da830e
Matthieu JACQUET <majacque@e1r5p18.42.fr>
2022-06-09 12:47:28 +0200

    add getPort() function

---
+++ includes/parsing_argv.hpp
@@ -0,0 +1,8 @@
+#ifndef PARSING_ARGV_HPP
+# define PARSING_ARGV_HPP
+
+#include <string>
+
+int	getPort(std::string s, unsigned int& port);
+
+#endif
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,13 +1,20 @@
 #include <cstdlib>
 #include <iostream>
 #include "color.h"
+#include "parsing_argv.hpp"
 
 int	main(int argc, char **argv){
-	(void)argv;
 	if (argc != 3){
 		std::cerr << "Error use ircserv like " GREEN "./ircserv <port> <password>" RESET << std::endl;
 		return (EXIT_FAILURE);
 	}
-	std::cerr << "Project is not working Yet " RED "We're Sorry" RESET << std::endl;
+	unsigned int	port;
+	std::string		password(argv[2]);
+
+	if (getPort(argv[1], port))
+		return (EXIT_FAILURE);
+	std::cout << "port = " << port << '\n';
+	std::cout << "password = " << password << '\n';
+	std::cout << "Project is not working Yet " RED "We're Sorry" RESET << std::endl;
 	return (EXIT_SUCCESS);
 }
