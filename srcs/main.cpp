cfc0e708226baea6a0e5102ab92649d3a4ba10f8
Florian CATINAUD <fcatinau@e1r11p5.42.fr>
2022-06-09 19:00:45 +0200

    Add note && start some test

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -3,6 +3,38 @@
 #include "color.h"
 #include "parsing_argv.hpp"
 
+#include <cerrno>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <cstdio>
+int	test_something_socket(void){
+	int fd_socket = socket(AF_LOCAL, SOCK_STREAM, 0);
+
+	if (fd_socket == -1){
+		perror("");
+		return (false);
+	}
+	std::cerr << "ret socket " << fd_socket << std::endl;
+	while (1);
+	return (true);
+}
+
+#include <signal.h>
+#include <poll.h>
+
+int	test_poll(void){
+	int	ret = poll(4, );// read man poll
+
+	if (ret == -1){
+		perror("");
+		return (false);
+	}
+	std::cerr << "ret poll " << ret << std::endl;
+	while (1);
+	return (true);
+}
+
 int	main(int argc, char **argv){
 	if (argc != 3){
 		std::cerr << "Error use ircserv like " GREEN "./ircserv <port> <password>" RESET << std::endl;
@@ -15,6 +47,13 @@ int	main(int argc, char **argv){
 		return (EXIT_FAILURE);
 	std::cout << "port = " << port << '\n';
 	std::cout << "password = " << password << '\n';
+
+	if (!test_something_socket())
+		return (EXIT_FAILURE);
+
+	if (!test_poll())
+		return (EXIT_FAILURE);
+
 	std::cout << "Project is not working Yet " RED "We're Sorry" RESET << std::endl;
 	return (EXIT_SUCCESS);
 }
