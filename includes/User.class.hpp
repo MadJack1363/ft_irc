30fbc90ca847f5c218f6dbca1f0ddc9184c25d47
Matthieu JACQUET <majacque@e1r12p17.42.fr>
2022-06-15 19:39:19 +0200

    begin data structur with Server, User, Channel class

---
+++ includes/User.class.hpp
@@ -0,0 +1,38 @@
+#ifndef USER_CLASS_HPP
+# define USER_CLASS_HPP
+
+#include <iostream>
+#include <string>
+#include <sys/types.h> // socket, bind, listen, recv, send
+#include <sys/socket.h>//   "      "      "      "     "
+#include <netinet/in.h>// sockaddr_in
+#include "Channel.class.hpp"
+
+class User {
+private:
+
+	int			_socket;
+	sockaddr_in	_addr;
+
+	std::string	_username;
+	std::string	_nickname;
+	std::string	_password;
+
+	bool		isOperator;
+
+public:
+
+	User( void );
+	User( User const & rhs );
+	virtual ~User( void );
+
+	User&	operator=( User const & rhs );
+
+	bool	init(int const & socket, sockaddr_in const & addr); // set _socket & _addr + fcntl() <-- setup non-blocking fd
+	void	disconnect( void );
+
+	bool	sendTo( User const & user ); // send private message
+	bool	sendToAll( Channel const & chan ); // send message to every user in the channel (except myself)
+};
+
+#endif
