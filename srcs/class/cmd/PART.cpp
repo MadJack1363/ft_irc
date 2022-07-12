73cbed49414bc59f3307400204a20bddfee96895
Florian Catinaud <catinaud.florian@gmail.com>
2022-07-12 18:15:01 +0200

    Little push for work on linux in school

--- srcs/class/cmd/PART.cpp
+++ srcs/class/cmd/PART.cpp
@@ -11,7 +11,7 @@
 bool	Server::PART(User &user, std::string &params)
 {
 	std::vector<std::string>	channel_left;
-	Channel						tmp;
+	// Channel						tmp;
 
 	Server::logMsg(RECEIVED, "(" + Server::toString(user.getSocket()) + ") PART " + params);
 	params = params.c_str() + params.find(':') + 1;
@@ -24,12 +24,25 @@ bool	Server::PART(User &user, std::string &params)
 	for(std::vector<std::string>::iterator ite = channel_left.begin();ite != channel_left.end();ite++)
 	{
 		// config for send a custom message or no of all user of any channel
-		tmp = this->_channels[*ite];
-		// if (tmp.getUsers().size() == 1)
-		// 	// need to delete the channel
-		// else{
-		// 	// send value to all User inside the channel
-		// }
+		// tmp = this->_channels[*ite];
+		// this->_channels[*ite].getUsers().erase();
+		if (this->_channels[*ite].getUsers().size() == 1)
+		{
+			this->_channels.erase(*ite);
+			// 	// need to delete the channel
+		}
+		else
+		{
+			std::vector<User *>::iterator	user_to_del;
+			for (user_to_del = this->_channels[*ite].getUsers().begin(); user_to_del != this->_channels[*ite].getUsers().end(); user_to_del++){
+				user_to_del
+			}
+			this->_channels[*ite].getUsers().erase(user_to_del);
+			ite->erase();
+			std::string	cpy = *ite + " has left the channel";
+			PRIVMSG(user, cpy);
+			// send value to all User inside the channel
+		}
 	}
 	return true;
 }
