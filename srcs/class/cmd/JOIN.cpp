e6d5cc1ea90d2832a5cacf6884c8297b9be4c36a
Florian CATINAUD <fcatinau@e2r2p2.clusters.42paris.fr>
2022-07-26 17:51:30 +0200

    Save the work before maintenance but need to rework on JOIN

--- srcs/class/cmd/JOIN.cpp
+++ srcs/class/cmd/JOIN.cpp
@@ -32,53 +32,51 @@ void	Server::joinSend(User &user, Channel &channel, std::string const &name_join
  */
 bool	Server::JOIN(User &user, std::string &params)
 {
-	std::vector<std::string>	channel_join;
+	// TODO Change the function for use iterator for all the fonctionnement
+	// std::vector<std::string>	channelToJoin; #test,#test1,#test2
 
-	std::string::const_iterator	start;
-	std::string::const_iterator	end = params.end();
-	for (start = params.begin(); start != end; start++)
-	{
-		if (*start == '#')
-			break ;
-	}
-	std::string	cpy = std::string(start, end);
-	// cpy = cpy.c_str() + cpy.find('#') + 1;
-	if (cpy.compare("0") == 0)
-		return this->PART(user, cpy);
-	while (cpy.find('#') != std::string::npos)
-	{
-		channel_join.push_back(cpy.substr(0, cpy.find(',')));
-		cpy = cpy.c_str() + cpy.find('#') + 1;
-	}
-	channel_join.push_back(cpy.substr(0, cpy.length()));
+	std::string					subParams;
+	std::string					channelJoin;
+	std::string::const_iterator	cit0 = params.begin();
+	std::string::const_iterator	cit1 = params.begin();
 
-	if (channel_join.empty())
-	{
-		return this->replyPush(user, ':' + user.getMask() + "461 JOIN :Not enough parameters");
-	}
-	for (std::vector<std::string>::iterator ite = channel_join.begin(); ite != channel_join.end(); ite++)
-	{
-		Channel &chan = this->_lookupChannels[*ite];
-		if (!user.getMsg().empty())
-			if (!this->replySend(user))
-				return false;
-		if (chan.getName().compare("Empty") == 0){
-			chan.setName(*ite);
-		}
-		chan.addUser(user);
-		std::string tmp = "#" + *ite;
-		for (std::vector<User *>::const_iterator itv = chan.getUsers().begin(); itv != chan.getUsers().end(); itv++)
-		{
-			if ((*itv)->getNickname().compare(user.getNickname()) == 0)
-				joinSend(*(*itv), chan, user.getNickname());
-			else
-			{
-				std::string tmp2 = ":" + user.getNickname() + "!" + user.getUsername() + "@" + this->_config["host"] + " IP JOIN :" + tmp;
-				(*itv)->setMsg(tmp);
-				this->replyPush(*(*itv), ':' + user.getMask() + "332 " + (*itv)->getNickname() + " " + tmp + " :" + user.getNickname() + " has joined the channel");
-			}
-			Server::replySend(*(*itv));
-		}
-	}
+
+	for (;cit1 != ":";cit1++);
+	channelJoin = std::string(cit0, cit1);
+	
+
+	// channelJoin = params.str(params.begin() , params.find(':'));
+
+	// if (channelToJoin.empty())
+	// {
+	// 	return this->replyPush(user, ':' + user.getMask() + "461 JOIN :Not enough parameters");
+	// }
+	// if (channelToJoin.find("0") != channelToJoin.end())// TODO check for discussion FOR THE error or NO
+	// 		return this->PART(user, *channelToJoin.find("0"));
+
+	// for (std::vector<std::string>::iterator ite = channelToJoin.begin(); ite != channelToJoin.end(); ite++)
+	// {
+	// 	Channel &chan = this->_lookupChannels[*ite];
+	// 	if (!user.getMsg().empty())
+	// 		if (!this->replySend(user))
+	// 			return false;
+	// 	if (chan.getName().compare("Empty") == 0){
+	// 		chan.setName(*ite);
+	// 	}
+	// 	chan.addUser(user);
+	// 	std::string tmp = "#" + *ite;
+	// 	for (std::vector<User *>::const_iterator itv = chan.getUsers().begin(); itv != chan.getUsers().end(); itv++)
+	// 	{
+	// 		if ((*itv)->getNickname().compare(user.getNickname()) == 0)
+	// 			joinSend(*(*itv), chan, user.getNickname());
+	// 		else
+	// 		{
+	// 			std::string tmp2 = ":" + user.getNickname() + "!" + user.getUsername() + "@" + this->_config["host"] + " IP JOIN :" + tmp;
+	// 			(*itv)->setMsg(tmp);
+	// 			this->replyPush(*(*itv), ':' + user.getMask() + "332 " + (*itv)->getNickname() + " " + tmp + " :" + user.getNickname() + " has joined the channel");
+	// 		}
+	// 		Server::replySend(*(*itv));
+	// 	}
+	// }
 	return true;
 }
